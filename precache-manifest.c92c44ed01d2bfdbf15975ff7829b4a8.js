8c7048de8fe5e8b614a006071d00305297614111
Tomasz Olędzki <luksfer@poczta.onet.pl>
2020-03-29 19:46:20 +0200

    Updates

---
+++ precache-manifest.c92c44ed01d2bfdbf15975ff7829b4a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fded7e3c4d94bf832bf95b16e864d1",
+    "url": "/sapporo/index.html"
+  },
+  {
+    "revision": "619a6ae6b427561b986e",
+    "url": "/sapporo/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "f17ce923611e48513875",
+    "url": "/sapporo/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "619a6ae6b427561b986e",
+    "url": "/sapporo/static/js/2.8cf5fc5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sapporo/static/js/2.8cf5fc5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17ce923611e48513875",
+    "url": "/sapporo/static/js/main.2d9b387b.chunk.js"
+  },
+  {
+    "revision": "d9b04185a1e61d2216ee",
+    "url": "/sapporo/static/js/runtime-main.7e9c9d87.js"
+  }
+]);
